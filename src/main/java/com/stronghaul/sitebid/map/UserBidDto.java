979c427ae3687bd441f16bf45258f586b2ebed3f
jpd0861 <jpd0861@gmail.com>
2026-09-08 15:58:48 -0600

    mapped bid status, next need line items

--- src/main/java/com/stronghaul/sitebid/map/UserBidDto.java
+++ src/main/java/com/stronghaul/sitebid/map/UserBidDto.java
@@ -11,9 +11,93 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stronghaul.sitebid.models.Address;
 import com.stronghaul.sitebid.models.UserBid;
+import com.stronghaul.sitebid.models.BidStatus;
 
 public class UserBidDto {
     private ArrayList<UserBid> userBids;
+
+    private JsonNode getJsonNode(JsonNode root, String key) {
+        JsonNode data = root.get(key);
+        if (data != null && !data.isNull()) {
+            return data;
+        }
+        return null;
+    }
+
+    private void mapBidRootValues(UserBid bid, JsonNode root) {
+        JsonNode node = getJsonNode(root, "bid_id");
+        if (node != null) {
+            bid.setId(node.asLong());
+        }
+
+        node = getJsonNode(root, "bid_scope_of_work");
+        if (node != null) {
+            bid.setScopeOfWork(node.asText());
+        }
+
+        node = getJsonNode(root, "bid_profit_percentage_override");
+        if (node != null) {
+            bid.setProfitPercentageOverride(new BigDecimal(node.asText()));
+        }
+
+        node = getJsonNode(root, "bid_date");
+        if (node != null) {
+            bid.setDateOfBid(LocalDateTime.parse(node.asText()));
+        }
+    }
+
+    private void mapBidAddress(UserBid bid, JsonNode root) {
+        JsonNode node = getJsonNode(root, "bidAddress");
+        if (node != null) {
+            Address address = new Address();
+            JsonNode addrNode = getJsonNode(node, "bid_addr_id");
+
+            if (addrNode != null) {
+                address.setId(addrNode.asLong());
+            }
+            addrNode = getJsonNode(node, "bid_addr_street");
+            if (addrNode != null) {
+                address.setStreet(addrNode.asText());
+            }
+            addrNode = getJsonNode(node, "bid_addr_zip");
+            if (addrNode != null) {
+                address.setZip(addrNode.asText());
+            }
+            bid.setAddress(address);
+        }             
+    }
+
+    private void mapBidStatus(UserBid bid, JsonNode root) {
+        JsonNode node = getJsonNode(root, "bidStatus");
+        if (node != null) {
+            BidStatus bidStatus = new BidStatus();
+            JsonNode statusNode = getJsonNode(node, "bid_stat_id");
+            if (statusNode != null) {
+                bidStatus.setId(statusNode.asLong());
+            }
+            statusNode = getJsonNode(node, "bid_stat_status");
+            if (statusNode != null) {
+                bidStatus.setStatus(statusNode.asText());
+            }
+            bid.setBidStatus(bidStatus);
+        }             
+    }
+
+    // private void mapLineItems(UserBid bid, JsonNode root) {
+    //     JsonNode node = getJsonNode(root, "lineItems");
+    //     if (node != null) {
+    //         BidStatus bidStatus = new BidStatus();
+    //         JsonNode statusNode = getJsonNode(node, "bid_stat_id");
+    //         if (statusNode != null) {
+    //             bidStatus.setId(statusNode.asLong());
+    //         }
+    //         statusNode = getJsonNode(node, "bid_stat_status");
+    //         if (statusNode != null) {
+    //             bidStatus.setStatus(statusNode.asText());
+    //         }
+    //         bid.setBidStatus(bidStatus);
+    //     }             
+    // }
 
     public ArrayList<UserBid> map(ResultSet resultSet) throws SQLException {
         this.userBids = new ArrayList<UserBid>();
@@ -23,59 +107,11 @@ public class UserBidDto {
             String json = resultSet.getString(1);
             try {
                 JsonNode root = mapper.readTree(json);
-
                 UserBid bid = new UserBid();
-                JsonNode rootBidId = root.get("bid_id");
-                if (rootBidId != null && !rootBidId.isNull()) {
-                    bid.setId(rootBidId.asLong());
-                }
 
-                JsonNode addressNode = root.get("bidAddress");
-                if (addressNode != null && !addressNode.isNull()) {
-                    Address bidAddress = new Address();
-                    JsonNode addrId = addressNode.get("bid_addr_id");
-                    if (addrId != null && !addrId.isNull()) {
-                        bidAddress.setId(addrId.asLong());
-                    }
-                    JsonNode addrStreet = addressNode.get("bid_addr_street");
-                    if (addrStreet != null && !addrStreet.isNull()) {
-                        bidAddress.setStreet(addrStreet.asText());
-                    }
-                    JsonNode addrZip = addressNode.get("bid_addr_zip");
-                    if (addrZip != null && !addrZip.isNull()) {
-                        bidAddress.setZip(addrZip.asText());
-                    }
-                    bid.setAddress(bidAddress);
-                }
-
-                JsonNode bidStatusNode = root.get("bidStatus");
-                if (bidStatusNode != null && !bidStatusNode.isNull()) {
-                    com.stronghaul.sitebid.models.BidStatus bidStatus = new com.stronghaul.sitebid.models.BidStatus();
-                    JsonNode statusId = bidStatusNode.get("bid_stat_id");
-                    if (statusId != null && !statusId.isNull()) {
-                        bidStatus.setId(statusId.asLong());
-                    }
-                    JsonNode statusText = bidStatusNode.get("bid_stat_status");
-                    if (statusText != null && !statusText.isNull()) {
-                        bidStatus.setStatus(statusText.asText());
-                    }
-                    bid.setBidStatus(bidStatus);
-                }
-
-                JsonNode scope = root.get("bid_scope_of_work");
-                if (scope != null && !scope.isNull()) {
-                    bid.setScopeOfWork(scope.asText());
-                }
-
-                JsonNode profit = root.get("bid_profit_percentage_override");
-                if (profit != null && !profit.isNull()) {
-                    bid.setProfitPercentageOverride(new BigDecimal(profit.asText()));
-                }
-
-                JsonNode bidDate = root.get("bid_date");
-                if (bidDate != null && !bidDate.isNull()) {
-                    bid.setDateOfBid(LocalDateTime.parse(bidDate.asText()));
-                }
+                mapBidRootValues(bid, root);
+                mapBidAddress(bid, root);
+                mapBidStatus(bid, root);
 
                 this.userBids.add(bid);
             } catch (JsonProcessingException ex) {
