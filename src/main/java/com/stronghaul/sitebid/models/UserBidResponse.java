63a8c1e3e2b2c393ec9e19e47fa4f4bfef6a70e9
jpd0861 <jpd0861@gmail.com>
2026-09-10 00:37:21 -0600

    setup calls for saving a user bid. Need more validation

--- src/main/java/com/stronghaul/sitebid/models/UserBidResponse.java
+++ src/main/java/com/stronghaul/sitebid/models/UserBidResponse.java
@@ -1,13 +1,20 @@
 package com.stronghaul.sitebid.models;
 
 public class UserBidResponse {
-    private String em = "";
-    private UserBid ub = new UserBid();
+    private String error = "";
+    private UserBid userBid = new UserBid();
 
-    public String getErrorMessage(){
-        return this.em;
+    public String getError(){
+        return this.error;
     }
+    public void setError(String value){
+        this.error = value;
+    }
+
     public UserBid getUserBid(){
-        return this.ub;
+        return this.userBid;
+    }
+    public void setUserBid(UserBid value){
+        this.userBid = value;
     }
 }
--- src/main/java/com/stronghaul/sitebid/services/UserBidService.java
+++ src/main/java/com/stronghaul/sitebid/services/UserBidService.java
@@ -2,8 +2,10 @@ package com.stronghaul.sitebid.services;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.stronghaul.sitebid.models.BidStatus;
 import com.stronghaul.sitebid.models.UserBid;
 import com.stronghaul.sitebid.models.UserBidRequest;
 import com.stronghaul.sitebid.models.UserBidResponse;
@@ -12,11 +14,8 @@ import com.stronghaul.sitebid.models.UserProfile;
 @Service 
 public class UserBidService {
 
-    private final PostgresDbService postgresDbService;
-
-    public UserBidService(PostgresDbService postgresDbService) {
-        this.postgresDbService = postgresDbService;
-    }
+    @Autowired private PostgresDbService postgresDbService;
+    @Autowired private ValidateUserBidRequest validateUserBidRequest;
 
     public ArrayList<UserBid> getUserBids(UserProfile userProfile, Long bidId) {
         return postgresDbService.getUserBids(userProfile, bidId);
@@ -24,6 +23,21 @@ public class UserBidService {
 
     public UserBidResponse createUserBid(UserBidRequest request){
         UserBidResponse response = new UserBidResponse();
+
+        if(!this.validateUserBidRequest.isValid(request)){
+            response.setError(validateUserBidRequest.getError());
+            return response;
+        }
+        
+        UserBid userBid = new UserBid();
+        userBid.setAddress(request.getSiteAddress());
+        userBid.setBidStatus(new BidStatus());
+        userBid.setDateOfBid(request.getDateOfBid());
+        userBid.setProfitPercentageOverride(request.getProfitPercentage());
+        userBid.setScopeOfWork(request.getScopeOfWork());
+        userBid.setDateOfBid(request.getDateOfBid());
+        userBid = this.postgresDbService.saveBid(userBid);
+        response.setUserBid(userBid);
         return response;
     }
 }
